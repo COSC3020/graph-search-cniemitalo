4c698e8cc3d7be18ce8ead4f858966eada819a01
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-03-16 07:59:26 -0600

    updates

--- code.js
+++ code.js
@@ -1,3 +1,42 @@
 function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+    //track which nodes have been visited and the path travelled
+    let visited = []; 
+    let path = []; 
+
+    //if node is in the graph, return the path
+    //else return empty array 
+    if (search(graph, startNode, targetNode, visited, path)) {
+        return path; 
+    } else {
+        return []; 
+    }
+
 }
+
+function search(graph, current, target, visited, path) {
+    //add the current node to visited and path
+    visited.push(current); 
+    path.push(current); 
+
+    if (current === target) {
+        return true; 
+    }
+
+    //search through the current nodes edges 
+    for (let i = 0; i < graph[current].length; i++) {
+        let node = graph[current][i]; 
+
+        //if the next edge hasn't already been visited
+        //recursively search through the graph for connected node 
+        if (!visited.includes(node) && search(graph, node, target, visited, path)) {
+            //return true to depthFirst search if the targetNode is found 
+            return true;
+        } 
+    }
+    
+    //delete the previous node from the path to backtrack 
+    path.pop(); 
+    return false;  
+}
+
+
