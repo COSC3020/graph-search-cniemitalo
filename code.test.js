fb4eb9f53c2c5716e3248feeab447561e5dbb846
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-03-26 15:45:34 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,43 +3,27 @@ const jsc = require('jsverify');
 
 eval(fs.readFileSync('code.js')+''); 
 
-let classGraph = [
-  [1,2,5],
-  [4],
-  [3,5],
-  [4],
-  [],
-  [6],
-  []
-];
+// used Ishita Patel's repository as inspiration for how to provide tests
 
-jsc.assert(JSON.stringify(depthFirstSearch(classGraph,0,3)) == JSON.stringify([0,2,3]));
+let emptyGraph = []; 
+let graph1 = [[1, 2], [3], [], []]; 
+let graph2 = [[1], [2], [0, 3], []]; 
+let graph3 = [[1, 2], [3], [3], []]; 
+let graph4 = [[1], [2], [], [4], []]; 
+let graph5 = [[0]]; 
+let graph6 = [[1, 2, 3], [4], [4, 5], [5], [6], [6], []]; 
 
-/*
-function convertListToMatrix(edges, maxNode) {
-    const mat = []; 
-    for (let i = 0; i <= maxNode; i++) {
-        for (let j = 0; j <= maxNode; j++) {
-            mat[i][j] = 0; 
-        }
-    }
-    edges.forEach(edge => {
-        mat[edge[0]][edge[1]] = 1; 
-    }); 
 
-    return mat; 
-}
-
-const test = jsc.forall("array (pair nat nat)", function(edges) {
-    let maxNode = edges.reduce(function(a, b) {return Math.max(a, Math.max(b[0], b[1])); }, 0); 
-    let adjMatrix = convertListToMatrix(edges, maxNode);
-
-    const startNode = Math.floor(Math.random() * (maxNode + 1)); 
-    const endNode = Math.floor(Math.random() * (maxNode + 1)); 
-    const result = depthFirstSearch(adjMatrix, startNode, endNode); 
-  
-    return typeof result === 'boolean'; 
-})
-
-jsc.assert(test, { tests: 1000 }); 
-*/
+jsc.assert(JSON.stringify(depthFirstSearch(emptyGraph, 0, 0)) == JSON.stringify([]));
+jsc.assert(JSON.stringify(depthFirstSearch(emptyGraph, 3, 7)) == JSON.stringify([]))
+jsc.assert(JSON.stringify(depthFirstSearch(graph1, 0, 3)) == JSON.stringify([0, 1, 3])); 
+jsc.assert(JSON.stringify(depthFirstSearch(graph1, 1, 2)) == JSON.stringify([]); 
+jsc.assert(JSON.stringify(depthFirstSearch(graph2, 0, 3)) == JSON.stringify([0, 1, 2, 3])); 
+jsc.assert(JSON.stringify(depthFirstSearch(graph2, 3, 0)) == JSON.stringify([])); 
+jsc.assert(JSON.stringify(depthFirstSearch(graph3, 2, 3)) == JSON.stringify([2, 3])); 
+jsc.assert(JSON.stringify(depthFirstSearch(graph3, 1, 0)) == JSON.stringify([]));  
+jsc.assert(JSON.stringify(depthFirstSearch(graph4, 0, 2)) == JSON.stringify([0, 1, 2])); 
+jsc.assert(JSON.stringify(depthFirstSearch(graph4, 3, 2)) == JSON.stringify([]); 
+jsc.assert(JSON.stringify(depthFirstSearch(graph5, 0, 0)) == JSON.stringify([0])); 
+jsc.assert(JSON.stringify(depthFirstSearch(graph6, 0, 6)) == JSON.stringify([0, 1, 4, 6]); 
+jsc.assert(JSON.stringify(depthFirstSearch(graph6, 5, 3)) == JSON.stringify([]); 
