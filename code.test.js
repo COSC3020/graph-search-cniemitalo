d661f8661e1494f4f471f3ab20c39429dd866b14
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-03-27 10:13:36 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,5 +1,5 @@
 const fs = require('fs'); 
-const jsc = require('jsverify'); 
+const assert = require('assert'); 
 
 eval(fs.readFileSync('code.js')+''); 
 
@@ -14,16 +14,16 @@ let graph5 = [[0]];
 let graph6 = [[1, 2, 3], [4], [4, 5], [5], [6], [6], []]; 
 
 
-jsc.assert(JSON.stringify(depthFirstSearch(emptyGraph, 0, 0)) == JSON.stringify([]));
-jsc.assert(JSON.stringify(depthFirstSearch(emptyGraph, 3, 7)) == JSON.stringify([]))
-jsc.assert(JSON.stringify(depthFirstSearch(graph1, 0, 3)) == JSON.stringify([0, 1, 3])); 
-jsc.assert(JSON.stringify(depthFirstSearch(graph1, 1, 2)) == JSON.stringify([])); 
-jsc.assert(JSON.stringify(depthFirstSearch(graph2, 0, 3)) == JSON.stringify([0, 1, 2, 3])); 
-jsc.assert(JSON.stringify(depthFirstSearch(graph2, 3, 0)) == JSON.stringify([])); 
-jsc.assert(JSON.stringify(depthFirstSearch(graph3, 2, 3)) == JSON.stringify([2, 3])); 
-jsc.assert(JSON.stringify(depthFirstSearch(graph3, 1, 0)) == JSON.stringify([]));  
-jsc.assert(JSON.stringify(depthFirstSearch(graph4, 0, 2)) == JSON.stringify([0, 1, 2])); 
-jsc.assert(JSON.stringify(depthFirstSearch(graph4, 3, 2)) == JSON.stringify([])); 
-jsc.assert(JSON.stringify(depthFirstSearch(graph5, 0, 0)) == JSON.stringify([0])); 
-jsc.assert(JSON.stringify(depthFirstSearch(graph6, 0, 6)) == JSON.stringify([0, 1, 4, 6])); 
-jsc.assert(JSON.stringify(depthFirstSearch(graph6, 5, 3)) == JSON.stringify([])); 
+assert(JSON.stringify(depthFirstSearch(emptyGraph, 0, 0)) == JSON.stringify([]));
+assert(JSON.stringify(depthFirstSearch(emptyGraph, 3, 7)) == JSON.stringify([]))
+assert(JSON.stringify(depthFirstSearch(graph1, 0, 3)) == JSON.stringify([0, 1, 3])); 
+assert(JSON.stringify(depthFirstSearch(graph1, 1, 2)) == JSON.stringify([])); 
+assert(JSON.stringify(depthFirstSearch(graph2, 0, 3)) == JSON.stringify([0, 1, 2, 3])); 
+assert(JSON.stringify(depthFirstSearch(graph2, 3, 0)) == JSON.stringify([])); 
+assert(JSON.stringify(depthFirstSearch(graph3, 2, 3)) == JSON.stringify([2, 3])); 
+assert(JSON.stringify(depthFirstSearch(graph3, 1, 0)) == JSON.stringify([]));  
+assert(JSON.stringify(depthFirstSearch(graph4, 0, 2)) == JSON.stringify([0, 1, 2])); 
+assert(JSON.stringify(depthFirstSearch(graph4, 3, 2)) == JSON.stringify([])); 
+assert(JSON.stringify(depthFirstSearch(graph5, 0, 0)) == JSON.stringify([0])); 
+assert(JSON.stringify(depthFirstSearch(graph6, 0, 6)) == JSON.stringify([0, 1, 4, 6])); 
+assert(JSON.stringify(depthFirstSearch(graph6, 5, 3)) == JSON.stringify([])); 
